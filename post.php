b1d670152f6c9cb61a9233cc535bbac74ef98e47
Misha2149796 <133091936+Misha2149796@users.noreply.github.com>
2023-11-24 12:33:18 +0100

    Add files via upload

---
+++ post.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <form method="POST" action="">
+            <input type="text" name="firstnamename" placeholder="Naam" required><br>
+            <input type="text" name="lastname" placeholder="Achternaam" required><br>
+            <input type="number" name="age" placeholder="Leeftijd" required><br>
+            <input type="text" name="adress" placeholder="Adres" required><br>
+            <input type="email" name="email" placeholder="Email" required><br>
+            <input type="submit" name="submit" value="Verstuur">
+        </form>
+
+        <?php 
+
+            if ((isset($_POST["submit"]))) {
+                echo "<br/>";
+
+                foreach ($_POST as $key => $value) {
+                    if ($key != "submit") {
+                        echo $key.": ".$value."<br/>";
+                    }
+                }
+            }
+        ?>
+    </body>
+</html>
